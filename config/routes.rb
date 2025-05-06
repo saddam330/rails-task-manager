57d892313693c7c1d23920dfb0e0b4f545de06e3
Saddam Hussain saddam5533@hotmail.com <saddam5533@hotmail.com>
2025-05-06 16:53:36 +0100

    As a user, I can add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new
   get 'tasks/:id', to: 'tasks#show', as: :show
+  post 'tasks', to: 'tasks#create'
 end
